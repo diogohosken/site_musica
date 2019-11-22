03f6db26f01b70ab37664e703a3fbdcadb621873
Diogo Hosken <diogoolivehosken@hotmail.com>
2019-11-22 08:33:13 -0300

    Desenvolvimento da area de contato

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 			<nav id="menuNav" class="navbar navbar-expand-lg justify-content-between align-items-center container">
 				<a class="navbar-brand" href="#">
-					<img src="assets/images/RaysProducoes.png" id="logo">
+					<img src="assets/images/RaysProducoes.png" id="logo" alt="logo">
 				</a>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expaded="false" aria-label="Toggle navigation">
@@ -47,19 +47,53 @@
 		</header>
 
 		<section id="sessaoSobre" class="container">
-			<h2 class="pt-3">SOBRE</h2>
+			<div class="row">
+				<h2 class="p-3">SOBRE</h2>
+			</div>
+			
 		</section>
 
 		<section id="sessaoMusicos" class="container">
-			<h2 class="pt-3">MUSICOS</h2>
+			<div class="row">
+				<h2 class="p-3">MUSICOS</h2>
+			</div>
 		</section>
 
 		<section id="sessaoGaleria" class="container">
-			<h2 class="pt-3">GALERIA</h2>
+			<div class="row">
+				<h2 class="p-3">GALERIA</h2>
+			</div>
 		</section>
 
 		<section id="sessaoContato" class="container">
-			<h2 class="pt-3">CONTATO</h2>
+			<div class="row">
+				<h2 class="p-3">CONTATO</h2>
+				<p class="display-3 p-3">Como entrar em contato conosco:</p>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3">
+					<p>Nome:</p>
+					<input type="text" name="nome"><br><br>
+
+					<p>email:</p>
+					<input type="text" name="nome"><br><br>
+
+					<p>Telefone ( ):</p>
+					<input type="text" name="nome"><br><br>
+
+					<p>Assunto:</p>
+					<input type="text" name="nome"><br><br>
+
+					<p>Mensagem:</p>
+					<textarea></textarea>
+				</div>
+
+				<div class="col-lg-9 p-4">
+					<img src="assets/images/mapa.jpg" style="width: 700px;" alt="mapa" />
+				</div>
+			</div>
+
 		</section>
 
 		<section id="sessao6" class="container">
@@ -67,7 +101,38 @@
 		</section>
 
 		<footer id="rodape" class="container">
-			<div class="row"></div>
+			<nav class="navbar navbar-expand-lg justify-content-between align-items-center">
+				<a class="navbar-brand" href="#">
+					<img src="assets/images/ray.png" style="width: 200px; margin: 55px 0 55px 0" alt="footer_logo" />
+				</a>
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expaded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggle-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="menu"></div>
+
+				<ul class="navbar-nav">
+					<li class="nav-item mr-3 h6">
+						<a href="" class="text-white">SOBRE</a>
+					</li>
+					<li class="nav-item mr-3 h6">
+						<a href="" class="text-white">MUSICOS</a>
+					</li>
+					<li class="nav-item mr-3 h6">
+						<a href="" class="text-white">GALERIA</a>
+					</li>
+					<li class="nav-item mr-3 h6">
+						<a href="" class="text-white">CONTATO</a>
+					</li>
+				</ul>
+			</nav>
+			<hr>
+			<div class="row">
+				<div class="col-lg-4">asdas</div>
+				<div class="col-lg-4">qqqwe</div>
+				<div class="col-lg-4">bcvbc</div>
+			</div>
 		</footer>
 
 	</div>
